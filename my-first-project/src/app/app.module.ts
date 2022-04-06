c3b52470c8d0d7d7c5bde244a5bd9dd45c88337c
computernerdzach <wizteczach@gmail.com>
2022-04-06 12:30:21 -0700

    component creation from cli and calling component from other component

--- my-first-project/src/app/app.module.ts
+++ my-first-project/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
