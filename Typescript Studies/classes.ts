aacc01a8bde63042fb7c89381d423b13183870dc
computernerdzach <wizteczach@gmail.com>
2022-04-06 09:22:15 -0700

    fix classes

--- Typescript Studies/classes.ts
+++ Typescript Studies/classes.ts
@@ -1,5 +1,5 @@
 
-class Car {
+export class Car {
 
     private brand: string;
     model: string;
@@ -7,21 +7,29 @@ class Car {
     yearManu: number;
 
     constructor(brand: string, model: string, color: string, yearManu: number) {
+
         this.brand = brand;
         this.model = model;
         this.color = color;
         this.yearManu = yearManu;
+
     }
 
     year() {
+
         console.log(this.yearManu);
         console.log(this.brand);
+
     }
 
-}
+    get getBrand() {
+        return this.brand;
+    }
+
+}}
 
 let car = new Car('mazda','rsx','yellow',2019);
-console.log(car.color + ' ' + car.brand);
+console.log(car.color + ' ' + car.getBrand);
 
 let car2 = new Car('ford','f150','brown',1981);
 console.log(car2.yearManu, car2.model);
