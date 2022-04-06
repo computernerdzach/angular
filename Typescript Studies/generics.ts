68cc1469862e92a50511dbe4a06ed48edc1981e1
computernerdzach <wizteczach@gmail.com>
2022-04-05 17:15:06 -0700

    generics

---
+++ Typescript Studies/generics.ts
@@ -0,0 +1,35 @@
+function result(x: number): number {
+    return x;
+}
+
+let y = result(2);
+console.log(y);
+
+function result2(x:string):string{
+    return x;
+}
+
+let z = result2('hi');
+console.log(z);
+
+function result3<T>(x:T):T{
+    return(x);
+}
+
+let zed = result3<number>(7);
+let yoga =result3<string>('yo momma');
+console.log(zed);
+console.log(yoga);
+
+let xray = result3(true);
+console.log(xray);
+
+class GenericClass<T>{
+    variable:T
+    y(parametre:T):T{
+        return parametre;
+    }
+}
+
+let wally = new GenericClass<number>()
+wally.y(5);
--- Typescript Studies/interfaces.js
+++ Typescript Studies/interfaces.js
@@ -1,15 +1,15 @@
-function save(person) {
-    console.log(person.username, 'has been registered');
-}
-save({ username: 'John', email: 'john@email', password: '123' });
-var Person2 = /** @class */ (function () {
-    function Person2() {
-    }
-    return Person2;
-}());
-var person2 = new Person2;
-person2.username;
-function save2(person2) {
-    console.log(person2.username, 'has been registered');
-}
-save2(person2);
+// function save(person) {
+//     console.log(person.username, 'has been registered');
+// }
+// save({ username: 'John', email: 'john@email', password: '123' });
+// var Person2 = /** @class */ (function () {
+//     function Person2() {
+//     }
+//     return Person2;
+// }());
+// var person2 = new Person2;
+// person2.username;
+// function save2(person2) {
+//     console.log(person2.username, 'has been registered');
+// }
+// save2(person2);
