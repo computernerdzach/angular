89b070e12359eee66905426c2bc35615f9b7c34c
computernerdzach <wizteczach@gmail.com>
2022-04-06 14:17:30 -0700

    event binding and summary of one-way binding

--- my-first-project/src/app/products/products.component.ts
+++ my-first-project/src/app/products/products.component.ts
@@ -9,11 +9,24 @@ import { Component } from "@angular/core";
 })
 export class ProductsComponent {
     addProduct=true;
+    addProductStatus='No product added';
+    greet='howdy ';
+    enteredValue='';
 
     constructor(){
         setTimeout(()=>{
             this.addProduct=false
         },4000)
+    }
+
+    onClick(){
+        this.addProductStatus='Product added';
+        console.log(this.greet);
+    }
+
+    onUpdateProductName(event:any){
+        console.log(event);
+        this.enteredValue=(<HTMLInputElement>event.target).value;
     }
 
 }
