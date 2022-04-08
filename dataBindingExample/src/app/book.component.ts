4a0390b9a595e749a595fd85aa735834e033ab4f
computernerdzach <wizteczach@gmail.com>
2022-04-08 11:59:14 -0700

    two way binding

--- dataBindingExample/src/app/book.component.ts
+++ dataBindingExample/src/app/book.component.ts
@@ -50,10 +50,10 @@ export class BookComponent {
         console.log('div was clicked');
     }
 
-    onKeyUp($event: any) {
 
-        console.log($event.target.value);
-
+    name = "John"
+    onKeyUp() {
+        console.log(this.name);
     }
 
 }
