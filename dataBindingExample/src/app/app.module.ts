5f4802641fd7329677f4f5bb46d86284006a1b61
computernerdzach <wizteczach@gmail.com>
2022-04-11 13:51:38 -0700

    custom directives

--- dataBindingExample/src/app/app.module.ts
+++ dataBindingExample/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BookComponent } from './book.component';
 import { CustomPipe } from './custom.pipe';
+import { ForEmailDirective } from './for-email.directive';
 
 
 @NgModule({
   declarations: [
     BookComponent,
-    CustomPipe
+    CustomPipe,
+    ForEmailDirective
   ],
   imports: [
     BrowserModule,
