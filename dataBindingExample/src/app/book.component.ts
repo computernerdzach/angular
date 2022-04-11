f332fb20f15190eaabbb6cc24084bfe97a94f54e
computernerdzach <wizteczach@gmail.com>
2022-04-11 10:07:38 -0700

    directives, ngIf, [hidden], and ng-template

--- dataBindingExample/src/app/book.component.ts
+++ dataBindingExample/src/app/book.component.ts
@@ -12,56 +12,62 @@ import { Book } from "./book.model";
 
 export class BookComponent {
 
-    name1:string='Angular Pipes';
-    price:number=159.9555;
-    today:number=Date.now();
-
-    description='Lorem ipsum lots of writing and stuff';
-
-
     model: BookRepository = new BookRepository();
-    book: Book = this.model.getBookId(1);
-
-    disabled = false;
-
-    getClassBinding(id: number): string {
-        let book = this.model.getBookId(id);
-        return (book.price! >= 20 ? "bg-primary " : "bg-info") + " m-3";
-    }
-
-    getClassObject(id: number): Object {
-        let book = this.model.getBookId(id);
-        return {
-            "bg-primary": book.price! >= 20,
-            "bg-info": book.price! < 20,
-            "text-white": book.name == "My Childhood"
-        }
-    }
-
-    color: string = this.model.getBookId(1).price! >= 20 ? "green" : "yellow";
-
-    getStyleBinding(id: number): Object {
-        let book = this.model.getBookId(id);
-        return {
-            fontSize: "40px",
-            color: "yellow"
-        }
-    }
-
-    onClick(event: any) {
-        event.stopPropagation();
-        console.log('Button was clicked');
-        console.log(event);
-    }
-
-    onClickDiv() {
-        console.log('div was clicked');
-    }
-
-
-    theName = "John"
-    onKeyUp() {
-        console.log(this.theName);
-    }
-
+    
 }
+
+// export class BookComponent {
+
+//     name1:string='Angular Pipes';
+//     price:number=159.9555;
+//     today:number=Date.now();
+
+//     description='Lorem ipsum lots of writing and stuff';
+
+
+//     model: BookRepository = new BookRepository();
+//     book: Book = this.model.getBookId(1);
+
+//     disabled = false;
+
+//     getClassBinding(id: number): string {
+//         let book = this.model.getBookId(id);
+//         return (book.price! >= 20 ? "bg-primary " : "bg-info") + " m-3";
+//     }
+
+//     getClassObject(id: number): Object {
+//         let book = this.model.getBookId(id);
+//         return {
+//             "bg-primary": book.price! >= 20,
+//             "bg-info": book.price! < 20,
+//             "text-white": book.name == "My Childhood"
+//         }
+//     }
+
+//     color: string = this.model.getBookId(1).price! >= 20 ? "green" : "yellow";
+
+//     getStyleBinding(id: number): Object {
+//         let book = this.model.getBookId(id);
+//         return {
+//             fontSize: "40px",
+//             color: "yellow"
+//         }
+//     }
+
+//     onClick(event: any) {
+//         event.stopPropagation();
+//         console.log('Button was clicked');
+//         console.log(event);
+//     }
+
+//     onClickDiv() {
+//         console.log('div was clicked');
+//     }
+
+
+//     theName = "John"
+//     onKeyUp() {
+//         console.log(this.theName);
+//     }
+
+// }
